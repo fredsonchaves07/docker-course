f2515c62beee90080f01d96f5326fa9ffae19188
Fredson Chaves <fredsonchaves07@gmail.com>
2020-11-30 08:18:13 -0300

    Atualizando arquivo dockerfile para nova versao

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_word():
-    return 'Hello Docker!'
+def mainpage():
+    name = 'Fredson'
+
+    render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
