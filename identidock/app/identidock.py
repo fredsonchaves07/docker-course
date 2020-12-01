a523bf95bf08f08f64720f5339df6d7757e89d20
Fredson Chaves <fredsonchaves07@gmail.com>
2020-12-01 08:15:08 -0300

    Ajustando erro do arquivo dockerfile

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def mainpage():
     name = 'Fredson'
 
-    render_template('index.html', name=name)
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- identidock/cmd.sh
+++ identidock/cmd.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-
 if [ "$ENV" = "DEV" ]; then
     echo "Iniciando em modo de desenvolvimento"
+    export FLASK_ENV=development && export FLASK_APP=identidock.py
     exec python "identidock.py"
 else
-    echo "Iniciando em modo de produÃ§Ã£o"
+    echo "Iniciando em modo de produção"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
 fi
