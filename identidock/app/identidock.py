fd72cada23438e46ea33d19e36eb7ef37c848988
Fredson Chaves <fredsonchaves07@gmail.com>
2020-12-03 08:15:51 -0300

    Adicao do container redis para armazenamento de cache

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import requests
+import hashlib
+import redis
 
 app = Flask(__name__)
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
+salt = "UNIQUE_SALT"
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def mainpage():
     name = 'Fredson'
+    if request.method == 'POST':
+        name = request.form['name']
+    salted_name = salt + name
+    name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
 
-    return render_template('index.html', name=name)
+    return render_template('index.html', name=name, name_hash=name_hash)
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-    r = requests.get(f'http://dnmonster:8080/monster/{name}?size=80')
-    image = r.content
+    image = cache.get(name)
+    if image is None:
+        print("Cache miss", flush=True)
+        r = requests.get(f'http://dnmonster:8080/monster/{name}?size=80')
+        image = r.content
+        cache.set(name, image)
 
     return Response(image, mimetype='image/png')
 
